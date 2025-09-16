897f49b5ef3a51c66df15c454f810e5f186c86c7
Kaique <kaique1245br@gmail.com>
2025-09-16 11:31:25 -0300

    Adicionado o css

--- php/adicionar_post.php
+++ php/adicionar_post.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/perfil.css">
-    <title>Perfil</title>
+    <link rel="stylesheet" href="../css/adicionar_post.css">
+    <title>Adicionar Post</title>
 </head>
 <body>
 
